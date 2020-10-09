0ed0901d91179d874f02d42073096ef8318b199f
Cherie Cookson <cheriecookson@gmail.com>
2020-10-08 21:39:30 -0500

    added facebook to footer and a few more edits to about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,10 +7,10 @@ function About() {
       <img src={coverImage} className="my-2" style={{ width: "20%" }} alt="cover" />
       <div className="my-2">
         <p>
-          When an analytical mind meets the hunger for creativity you get a Certified Full Stack Web Developer with a Masters of Accounting.
+          When an analytical mind meets creativity you get a Certified Full Stack Web Developer with a Masters of Accounting.
         </p>
         <p>
-          I started my career selling total IT solutions to small and medium sized businesses at Dell Inc after graduating from Texas Tech University with a BS in Communication Studies in 2004.  Five years later, during the financial crisis of 2009, I left Dell Inc to pursue a Masters of Accounting degree from Texas A&M Corpus Christi.  While at TAMUCC I was awarded a Graduate Assistantship at the university’s Business Innovation Center where I helped start up businesses put their ideas into motion.
+          I started my career selling total IT solutions to small and medium sized businesses at Dell Inc after graduating from Texas Tech University with a BS in Communication Studies in 2004.  Five years later, during the financial crisis of 2009, I left Dell Inc to pursue a Masters of Accounting degree from Texas A&M Corpus Christi.  While at TAMUCC I was awarded a Graduate Assistantship at the university’s Business Innovation Center where I helped startup businesses put their ideas into motion.
         </p>
         <p>
           Fast forward to today, I am now a full time Accountant working for one of the top Cardiothoracic Surgeons in South Texas.  I am also the sole Accountant for two other up and coming businesses.
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,11 +7,12 @@ const Footer = () => (
                 <a href="https://github.com/cheriecookson"><i class="fab fa-github"></i></a>
             </li>
             <li>
-                <a href="./assets/cv.pdf"><i class="fab fa-instagram"></i></a>
-            </li>
-            <li>
                 <a href="https://www.linkedin.com/in/cherie-cookson-3501646/"><i class="fab fa-linkedin"></i></a>
             </li>
+            <li>
+                <a href="https://www.facebook.com/cherie.cookson/"><i class="fab fa-facebook-square"></i></a>
+            </li>
+            
             
         </ul>
 
