8879db08490e6ca2152b3abfc4845a8476eef068
Cherie Cookson <cheriecookson@gmail.com>
2020-10-08 19:16:00 -0500

    typo fix

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,10 +10,10 @@ function About() {
           When an analytical mind meets the hunger for creativity you get a Certified Full Stack Web Developer with a Masters of Accounting.
         </p>
         <p>
-          I started my career selling total IT solutions for small and medium sized businesses at Dell Inc after graduating from Texas Tech University with a BS in Communication Studies in 2004.  Five years later, during the financial crisis of 2009, I left Dell Inc to pursue a Masters of Accounting degree from Texas A&M Corpus Christi.  While at TAMUCC I was awarded a Graduate Assistantship at the university’s Business Innovation Center where I helped start up businesses put their ideas into motion.
+          I started my career selling total IT solutions to small and medium sized businesses at Dell Inc after graduating from Texas Tech University with a BS in Communication Studies in 2004.  Five years later, during the financial crisis of 2009, I left Dell Inc to pursue a Masters of Accounting degree from Texas A&M Corpus Christi.  While at TAMUCC I was awarded a Graduate Assistantship at the university’s Business Innovation Center where I helped start up businesses put their ideas into motion.
         </p>
         <p>
-          Fast forward to today, I am now a full time Accountant working for one of the top Cardiothoracic Surgeons in South Texas.  As the sole Accountant, I oversee all financial, tax and HR activities on a daily basis. And as if that didn’t give me enough to do, I am also the sole Accountant for two other up and coming businesses.
+          Fast forward to today, I am now a full time Accountant working for one of the top Cardiothoracic Surgeons in South Texas.  I am also the sole Accountant for two other up and coming businesses.
           </p>
         <p>
           My Accounting career has been reliable and rewarding but I desire something more where I can let my creative side thrive.  As a Full Stack Web Developer I will get the best of both worlds.
